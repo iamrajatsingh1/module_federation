f17e68337dcd6f8641deb3d5fd349fb93c6ef7f0
iamrajatsingh1 <iamrajatsingh1@gmail.com>
2025-02-06 01:31:56 +0530

    feat: explored module federation with two react apps with same and diff react versions

---
+++ with_diff_react_vers/app2/webpack.config.js
@@ -0,0 +1,58 @@
+// app2/config/webpack.config.js
+
+const { ModuleFederationPlugin } = require('webpack').container;
+const { DefinePlugin } = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const dotenv = require('dotenv');
+const path = require("path");
+const dependencies = require("./package.json").dependencies;
+
+dotenv.config();
+
+module.exports = function () {
+  return {
+    entry: './src/index.js',
+    mode: "development",
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+      ],
+    },
+    plugins: [
+      new DefinePlugin({
+        'process.env.PUBLIC_URL': JSON.stringify(process.env.PUBLIC_URL || '/'),
+      }),
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+      new ModuleFederationPlugin({
+        name: 'app2',
+        remotes: {
+          app1: 'app1@http://localhost:3001/remoteEntry.js',
+        },
+        shared: {
+          react: { singleton: true, eager: true, requiredVersion: dependencies["react"] },
+          'react-dom': { singleton: true, eager: true, requiredVersion: dependencies["react-dom"] },
+        },
+      }),
+    ],
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      publicPath: "http://localhost:3002/",
+    },
+    devServer: {
+      port: 3002,
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+      hot: true,
+      historyApiFallback: true,
+    },
+  };
+};
